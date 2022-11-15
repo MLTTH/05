f8a1da51be31ff19390498a71aaae992cf7537b8
mltth <mltth.dev@gmail.com>
2022-11-15 17:44:22 +0100

    database connection, empty homepage template added

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+// We connect to the database.
+try {
+	$database = new PDO('mysql:host=localhost;dbname=05;charset=utf8', '05', 'password');
+} catch(Exception $e) {
+	die('Erreur : '.$e->getMessage());
+}
+
+// We retrieve the 5 last blog posts.
+$statement = $database->query(
+	"SELECT id, titre, contenu, DATE_FORMAT(date_creation, '%d/%m/%Y à %Hh%imin%ss') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5"
+);
+$posts = [];
+while (($row = $statement->fetch())) {
+	$post = [
+    	'title' => $row['titre'],
+    	'french_creation_date' => $row['date_creation_fr'],
+    	'content' => $row['contenu'],
+	];
+
+	$posts[] = $post;
+}
+
+require('templates/homepage.php');
+?>
